f9896ee5991b8206724f2739172b9842d8e5a42f
rpaladin <129979622+tnguyen-win@users.noreply.github.com>
2023-05-12 21:49:04 -0700

    Lab 5 - bookmarker

--- src/js/indexStart.js
+++ src/js/indexStart.js
@@ -1,90 +1,114 @@
 /* jshint esversion: 10 */
-
 class Bookmarker {
 	constructor() {
 		try {
 			this.bookmarks = JSON.parse(localStorage.bookmarks);
 		} catch {
-			this.bookmarks = [
-				{ description: "DESCRIPTION GOES HERE", image: "./images/bookmark.png", link: "", title: "TITLE GOES HERE" },
-			];
+			this.bookmarks = [{
+				description: "DESCRIPTION GOES HERE",
+				image: "./images/bookmark.png",
+				link: "",
+				title: "TITLE GOES HERE"
+			},];
 		}
 
 		this.addEventHandlers = this.addEventHandlers.bind(this);
 		this.fillBookmarks();
 		this.addBookmark = this.addBookmark.bind(this);
+		this.apiUrl = "https://opengraph.io/api/1.1/site";
+		this.appId = "fbc0d09c-b334-455b-aa86-bcb0a1714968";
 
 		document.getElementsByClassName("floater-bottom")[0].children[0].onclick = this.addBookmark;
 	}
 
-	addBookmark() {
-		const url = document.getElementById("url");
-		const desc = document.getElementById("description");
-		const urlV = url.value;
-		const descV = desc.value;
+	addBookmark(e) {
+		e.preventDefault();
 
-		if (urlV == "") url.classList.add("is-invalid");
-		if (descV == "") desc.classList.add("is-invalid");
-		else if (urlV != "" && descV != "") {
-			url.classList.remove("is-invalid");
-			desc.classList.remove("is-invalid");
+		const eUrl = document.getElementById("url");
+		const eDesc = document.getElementById("description");
+		const vUrl = eUrl.value;
+		const vDesc = eDesc.value;
+		const uURL = encodeURIComponent(vUrl);
 
-			const newBookmark = {
-				description: descV,
-				image: "IMAGE URL GOES HERE",
-				link: urlV,
-				title: "TITLE GOES HERE"
-			};
+		if (vUrl == "") eUrl.classList.add("is-invalid");
+		if (vDesc == "") eDesc.classList.add("is-invalid");
 
-			this.bookmarks.push(newBookmark);
-			this.fillBookmarks();
+		if (vUrl != "" && vDesc != "") {
+			eUrl.classList.remove("is-invalid");
+			eDesc.classList.remove("is-invalid");
 
-			url.value = "";
+			fetch(`${this.apiUrl}/${uURL}?app_id=${this.appId}`)
+				.then((res) => res.ok ? res.json() : (() => { throw new Error(res.status); })())
+				.then(data => {
+					const newBookmark = {
+						description: vDesc,
+						image: data.hybridGraph.image,
+						link: vUrl,
+						title: data.hybridGraph.title
+					};
+
+					this.bookmarks.push(newBookmark);
+					this.fillBookmarks();
+
+					document.querySelector(".bookmark-form").reset();
+				})
+				.catch(e => {
+					alert("There was a problem getting information. See the console for further details.");
+					console.log(`FETCH ERROR: ${e}`);
+				});
+
 		}
 	}
 
 	addEventHandlers() {
 		const deleteIcons = document.getElementsByName("deleteBookmark");
+		const formInputs = document.getElementsByTagName("input");
 
-		deleteIcons.forEach((d, i) => { d.onclick = this.deleteBookmark.bind(this, i); });
+		deleteIcons.forEach((d, i) => d.onclick = this.deleteBookmark.bind(this, i));
+
+		for (let d in formInputs) if (formInputs[d].tagName === "INPUT") formInputs[d].onfocus = () => document.body.classList.add("show-floater");
+
+		document.onclick = e => {
+			if (!e.target.closest("#url") && !e.target.closest("#description")) document.body.classList.remove("show-floater");
+		};
 	}
 
 	fillBookmarks() {
 		localStorage.bookmarks = JSON.stringify(this.bookmarks);
 
-		let bookmarksHtml = this.bookmarks.reduce((html, bookmark) => html += this.generateBookmarkHtml(bookmark), "");
+		let bookmarksHtml = this.bookmarks.reduce((h, b) => h += this.generateBookmarkHtml(b), "");
 
 		document.getElementsByClassName("bookmarks-list")[0].innerHTML = bookmarksHtml;
 
 		this.addEventHandlers();
 	}
 
-	generateBookmarkHtml(bookmark) {
+	generateBookmarkHtml(b) {
 		return `
-			<a href="${bookmark.link}" target="_blank" class="bookmark">
-				<div class="img" style='background-image: url(${bookmark.image})'>
-					&nbsp;
+		<a href="${b.link}" target="_blank" class="bookmark">
+			<div class="img" style='background-image: url("${b.image}")'>
+				&nbsp;
+			</div>
+			<div class="title">
+				${b.title}
+				<br>
+				${b.description}
 				</div>
-				<div class="title">
-					${bookmark.title}
-					<br>
-					${bookmark.description}
-					</div>
-				<div class="ms-auto">
-					<i name="deleteBookmark" class="bi-trash delete-icon"></i>
-				</div>
-			</a>
+			<div class="ms-auto">
+				<i name="deleteBookmark" class="bi-trash delete-icon"></i>
+			</div>
+		</a>
 		`;
 	}
 
-	deleteBookmark(index, event) {
-		event.preventDefault();
+	deleteBookmark(i, e) {
+		e.preventDefault();
 
-		this.bookmarks.splice(index, 1);
+		this.bookmarks.splice(i, 1);
 		this.fillBookmarks();
 	}
 }
 
 var bookmark;
 
-window.onload = () => { bookmark = new Bookmarker(); };
+window.onload = () => bookmark = new Bookmarker();
